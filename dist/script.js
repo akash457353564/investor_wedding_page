45256b449be8033f08a418e7a71173bf9aeca3e8
akash457353564 <102581699+akash457353564@users.noreply.github.com>
2023-05-09 18:21:53 +0530

    commit

--- dist/script.js
+++ dist/script.js
@@ -557,6 +557,33 @@ function hmrAccept(bundle, id) {
 }
 
 },{}],"6rimH":[function(require,module,exports) {
+const calendar_node = document.querySelector(".calender");
+const calendar = new FullCalendar.Calendar(calendar_node, {
+    initialView: "dayGridMonth",
+    headerToolbar: {
+        left: "prev",
+        center: "title",
+        right: "next"
+    },
+    events: [
+        {
+            title: "Booked",
+            date: "2023-05-09",
+            backgroundColor: "#FF6296"
+        },
+        {
+            title: "Booked",
+            date: "2023-05-10",
+            backgroundColor: "#FF6296"
+        },
+        {
+            title: "Booked",
+            date: "2023-05-13",
+            backgroundColor: "#FF6296"
+        }
+    ]
+});
+calendar.render();
 
 },{}]},["dpgAG","6rimH"], "6rimH", "parcelRequire1878")
 
--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 
+const calendar_node = document.querySelector('.calender')
+
+const calendar = new FullCalendar.Calendar(calendar_node, {
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+        left: 'prev',
+        center: 'title',
+        right: 'next'
+    },
+    events: [
+        {
+            title: 'Booked',
+            date: '2023-05-09',
+            backgroundColor: '#FF6296'
+        },
+        {
+            title: 'Booked',
+            date: '2023-05-10',
+            backgroundColor: '#FF6296'
+        },
+        {
+            title: 'Booked',
+            date: '2023-05-13',
+            backgroundColor: '#FF6296'
+        }
+    ]
+  });
+  calendar.render();
